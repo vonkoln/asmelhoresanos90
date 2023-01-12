1e9a0ab9f2abb74df56b6faf747be0165b129d11
brunovonkoln <brunovonkoln@gmail.com>
2023-01-12 11:44:48 -0300

    ..

--- data.js
+++ data.js
@@ -2,13 +2,13 @@ export default [
   {
     title:"No Matter What I Do [It's All About You] (Rock Da Planet Mix)",
     artist: "Alexia",
-    cover: "hipo.png",
+    cover: "hipo.jpg",
     file: "Alexia-NoMatterWhatIDo[It'sAllAboutYou](RockDaPlanetMix).mp3",
   },
   {
     title:"Schöneberg (Moonmen Over Marmion Remix)",
     artist: "Marmion",
-    cover: "hipo2.png",
+    cover: "hipo2.jpg",
     file: "Marmion-Schöneberg(MoonmenOverMarmionRemix).mp3",
   },
   
