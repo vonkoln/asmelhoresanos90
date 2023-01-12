0dd14d56fc34942c0a51827860cd5ed9791d007e
brunovonkoln <brunovonkoln@gmail.com>
2023-01-12 11:27:08 -0300

    ..

---
+++ data.js
@@ -0,0 +1,93 @@
+export default [
+  {
+    title:"Straight Down On The Floor",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "01-DJAlberto-StraightDownOnTheFloor.mp3",
+  },
+  {
+    title:"The DJ Rocks",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "02-DJAlberto-TheDJRocks.mp3",
+  },
+  {
+    title:"Control (Phoenix Version)",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "03-DJAlberto-Control(PhoenixVersion).mp3",
+  },
+  {
+    title:"Donklap",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "04-DJAlberto-Donklap.mp3",
+  },
+  {
+    title:"Listen",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "05-DJAlberto-Listen.mp3",
+  },
+  {
+    title:"Party Time",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "06-DJAlberto-PartyTime.mp3",
+  },
+  {
+    title:"On The Beat",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "07-DJAlberto-OnTheBeat.mp3",
+  },
+  {
+    title:"Heartbreaker",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "08-DJAlberto-Heartbreaker.mp3",
+  },
+  {
+    title:"Do That Shit",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "09-DJAlberto-DoThatShit.mp3",
+  },
+  {
+    title:"Trip",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "10-DJAlberto-Trip.mp3",
+  },
+  {
+    title:"The Music Flows",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "11-DJAlberto-TheMusicFlows.mp3",
+  },
+  {
+    title:"Blow Up My Mind",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "12-DJAlberto-BlowUpMyMind.mp3",
+  },
+  {
+    title:"Concept Motion Megamix",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "13-DJAlberto-ConceptMotionMegamix.mp3",
+  },
+  {
+    title:"On The Beat (House Version)",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "14-DJAlberto-OnTheBeat(HouseVersion).mp3",
+  },
+  {
+    title:"Technoises",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "15-DJAlberto-Technoises.mp3",
+  },
+  
+];
