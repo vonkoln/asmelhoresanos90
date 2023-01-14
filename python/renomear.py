0566a2862516e29d26dc900b10b64325cc5bf672
brunovonkoln <brunovonkoln@gmail.com>
2023-01-14 19:59:12 -0300

    ..

--- python/renomear.py
+++ python/renomear.py
@@ -4,11 +4,11 @@ caminho = r"C:\dev\alea\files"
 os.chdir(caminho)
 
 for f in os.listdir():
-   f_name, f_ext = os.path.splitext(f)
+    f_name, f_ext = os.path.splitext(f)
 
 
-   f_name = f_name.replace(' ','')
-   
-   new_name = f_name
+    f_name = f_name.replace(' ', '')
 
-   os.rename(f, new_name)
+    new_name = f_name
+
+    os.rename(f, new_name)
