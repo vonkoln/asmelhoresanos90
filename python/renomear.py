6d926638e1a13fe117e7ef634d2a41b6c3266c23
brunovonkoln <brunovonkoln@gmail.com>
2023-01-14 19:27:06 -0300

    ..

--- python/renomear.py
+++ python/renomear.py
@@ -6,14 +6,9 @@ os.chdir(caminho)
 for f in os.listdir():
    f_name, f_ext = os.path.splitext(f)
 
-#    f_erro, f_num, f_nome, f_autor = f_name.split('-')
+
    f_name = f_name.replace(' ','')
    
    new_name = f_name
 
    os.rename(f, new_name)
-
-
-
-    # f'{f_nome}-{f_autor}{f_ext}'
-    #   teste = os.path.splitext(f).replace(' ','')
